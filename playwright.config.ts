21c62d2103ce324fcaa171db8e57fd59a8f3dbc2
tastley-Urbanzoo <thomasastley@urbanzoo.io>
2025-09-18 09:39:26 +0100

    Updated reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,14 +4,16 @@ dotenv.config(); // ✅ Load environment variables from .env
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './test',  // Adjust if your test directory is different
+  testDir: './test/CMS',  // Adjust if your test directory is different
   testIgnore: ['**/EFL/**'],  // Ignore the EFL folder
 
   timeout: 30000,
   expect: {
     timeout: 5000,
   },
-  reporter: 'html', // Generates an HTML report
+  
+  reporter: [['junit', { outputFile: 'results.xml' }]],
+  //reporter: 'html', // Generates an HTML report
   use: {
     trace: 'on-first-retry', // Enables tracing for debugging
   },
